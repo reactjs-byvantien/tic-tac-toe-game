22f70b52dbe707096dafcabda5c922c87f93b9ac
tienpham <tpha768@gmail.com>
2026-05-11 12:48:41 +0700

    optimize: func handleSelectSquare

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,7 +5,13 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
   // const handleSelectBoard = (rowIndex, colIndex) => {
   //   setGameBoard((prevGameBoard) => {
@@ -16,7 +22,7 @@ export default function GameBoard({ onSelectSquare }) {
   //     return updatedGameBoard;
   //   });
   //   onSelectSquare();
-  };
+  // };
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -24,7 +30,11 @@ export default function GameBoard({ onSelectSquare }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>
+                <button
+                  onClick={() => {
+                    onSelectSquare(rowIndex, colIndex);
+                  }}
+                >
                   {playerSymbol}
                 </button>
               </li>

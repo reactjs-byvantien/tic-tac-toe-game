5543ac8b5f5144b67091a7b250f0785916f6fefc
tienpham <tpha768@gmail.com>
2026-05-12 18:09:57 +0700

    add: outsourcing data into a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning_combinations";
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "x";

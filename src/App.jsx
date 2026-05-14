d9e7aad9cba4dba753036e34b35af9a5ba3d6e78
tienpham <tpha768@gmail.com>
2026-05-15 00:06:10 +0700

    optimize: constructure project folder and file

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,14 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
-import { WINNING_COMBINATIONS } from "./winning_combinations";
 import GameOver from "./components/GameOver";
+import { PLAYERS } from "./constants/UsernamePlayer";
+import {
+  deriveActivePlayer,
+  deriveGameBoard,
+  deriveWinner,
+} from "./utils/derive";
 
-const PLAYERS = {
-  x: "Player 1",
-  o: "Player 2",
-};
-const INITIAL_GAME_BOARD = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-function deriveActivePlayer(gameTurns) {
-  let currentPlayer = "x";
-  if (gameTurns.length > 0 && gameTurns[0].player === "x") {
-    currentPlayer = "o";
-  }
-  return currentPlayer;
-}
-function deriveGameBoard(gameTurns) {
-  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
-  return gameBoard;
-}
-function deriveWinner(gameBoard, players) {
-  let winner;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].col];
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
-    console.log(firstSquareSymbol, secondSquareSymbol, thirdSquareSymbol);
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
-    }
-  }
-  return winner;
-}
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const [players, setPlayers] = useState(PLAYERS);
---
+++ src/constants/UsernamePlayer.js
@@ -0,0 +1,4 @@
+export const PLAYERS = {
+  x: "Player 1",
+  o: "Player 2",
+};
